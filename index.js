6207859ff88e89d02a2d624171ddb713c0d14476
Ansh Vijay <anshvijay28@gmail.com>
2023-02-08 12:46:05 -0500

    got started on logic to make sure not more than one operation can be pressed in a row without evaluation first

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
-inputs = document.querySelectorAll('.input');
-screen = document.querySelector('.screen');
-equalsButton = document.querySelector('#equals');
-clearButton = document.querySelector('#AC');
-inputted = document.querySelector('.incoming-operation');
+const inputs = document.querySelectorAll('.input');
+const operators = document.querySelectorAll('.operator');
+const numbers = document.querySelectorAll('.number');
 
-inputs.forEach(input => input.addEventListener('click', e => {
-    inputted.textContent += input.textContent;
+const equalsButton = document.querySelector('#equals');
+const clearButton = document.querySelector('#AC');
+
+const screen = document.querySelector('.screen');
+const incoming = document.querySelector('.incoming-operation');
+const output = document.querySelector('.result');
+
+let operationPressed = false;
+
+inputs.forEach(input => input.addEventListener('click', e => {  
+    if (input.classList.contains("operator")) {
+        if (operationPressed == false) {
+            incoming.textContent += input.textContent; 
+        }
+    } else /*if number*/ {
+        incoming.textContent += input.textContent;
+    }    
+}));
+operators.forEach(operator => operator.addEventListener('click', e => {
+    if (!operationPressed) {
+        operationPressed = true;
+    } else {
+        //evaluate expression
+        //clear incoming screen
+        //put result onto output screen
+        //make operationPressed = false 
+    }
 }));
 clearButton.addEventListener('click', e => {
-    inputted.textContent = "";
+    incoming.textContent = "";
+    output.textContent = "";
+    operationPressed = false;
 });
+equalsButton.addEventListener('click', e => {
+    incoming.textContent = "";
+    operationPressed = false;
+});
+
 
 
 
